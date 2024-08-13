bc7bc1da829f47b9105db59d7a6737bc56b6244f
DemboToure <dembotoure00@gmail.com>
2024-08-13 16:42:32 +0100

    first commit

---
+++ src/main/java/com/techie/microservices/inventory_service/model/Inventory.java
@@ -0,0 +1,23 @@
+package com.techie.microservices.inventory_service.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_inventory")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
---
+++ src/main/java/com/techie/microservices/inventory_service/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.techie.microservices.inventory_service.service;
+
+
+import com.techie.microservices.inventory_service.model.Inventory;
+import com.techie.microservices.inventory_service.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    public boolean isInStock(String skuCode, Integer quantity) {
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+    }
+}
